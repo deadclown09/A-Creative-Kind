4cf7b02b31d828da9bd9597205a61e94eeb8f918
Sandesh Katwal <mesandesh.2014@gmail.com>
2023-06-09 10:28:02 +0545

    feat: created provider component to wrap the app for authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Nav from "@components/Nav";
 
 import "@styles/globals.css";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "A Creative Kind",
@@ -12,13 +13,15 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
