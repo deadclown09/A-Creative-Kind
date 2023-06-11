be35119670d172f6d6abe3e1dec0417047c372cc
Sandesh Katwal <mesandesh.2014@gmail.com>
2023-06-11 12:26:17 +0545

    feat: created loader component for animation

---
+++ components/Loader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="h-5 w-5 rounded-full border-4 border-slate-200 border-b-transparent animate-spin"></div>
+  );
+};
+
+export default Loader;
