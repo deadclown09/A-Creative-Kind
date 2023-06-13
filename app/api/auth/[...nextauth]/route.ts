5d31b51abdd4172c13c259fc7b3d14cced192d92
Sandesh Katwal <mesandesh.2014@gmail.com>
2023-06-13 10:58:11 +0545

    feat: created unique usernames for each users

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -34,7 +34,7 @@ const handler = NextAuth({
         if (!userExists) {
           await User.create({
             email: profile.email,
-            username: profile.name.replace(/\s+\g/, "").toLowerCase(),
+            username: profile.email.replace("@gmail.com", ""),
             image: profile.picture,
           });
         }
